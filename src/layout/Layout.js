50017605b98eb2c600212a3c4fe4638e93e30720
tatynaerkinbek <tatynaerkinbek@gmail.com>
2023-02-07 17:21:14 +0600

    ff

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
+
 const Layout = ({children}) => {
 
     return (
         <div>
+
             <Header/>
             {children}
             <Footer/>
