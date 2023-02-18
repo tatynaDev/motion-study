efc3fa84268cdd5a3b5ff0c6035a761309d031d0
Tatyna <111748254+tatynaDev@users.noreply.github.com>
2023-02-18 17:28:55 +0600

    uni (#16)

--- src/components/StudyAbroad/countries/UnitedStates.js
+++ src/components/StudyAbroad/countries/UnitedStates.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import img1 from '../../../img/first.svg'
 import img2 from '../../../img/second.svg'
 import img3 from '../../../img/third.svg'
@@ -8,9 +8,12 @@ import img6 from '../../../img/sixs.svg'
 import img7 from '../../../img/sevens.svg'
 import {TfiLocationPin} from "react-icons/tfi";
 import {useNavigate} from "react-router-dom";
+import {MdKeyboardArrowDown, MdKeyboardArrowUp} from "react-icons/md";
 
 const UnitedStates = () => {
     const navigate = useNavigate()
+    const [opened, setOpened] = useState('')
+
     const universities = [
         {img: img1, title: 'Solbrige University', loc: 'Perth', acc: 'From 16'},
         {img: img2, title: 'La Trobe University', loc: 'Riga', acc: 'From 16'},
@@ -27,12 +30,53 @@ const UnitedStates = () => {
                 <div className="states">
                     <h1>United States</h1>
                     <div className='states--edu'>
-                        <select>
-                            <option>Higher Education</option>
-                        </select>
-                        <select>
-                            <option>specialization</option>
-                        </select>
+                        <div className='sel'>
+                            <h5 onClick={() => setOpened('1')}
+                                className='select'>Higher Education <span>{ opened === '1' ? <MdKeyboardArrowDown/> : <MdKeyboardArrowUp/>}</span></h5>
+                            <div className="option" style={{
+                                transition: '.2s',
+                                display: opened === '1' ? 'block' : "none"
+                            }}>
+                                <option>Higher Education</option>
+                                <option>Secondary education</option>
+                                <option>Higher education</option>
+                                <option>Languages courses for youth</option>
+                                <option>Children's language camps</option>
+                            </div>
+                        </div>
+
+                        <div className='sel'>
+                            <h5 onClick={() => setOpened('2')}
+                                className='select'>Specialization <span>{ opened === '2' ? <MdKeyboardArrowDown/> : <MdKeyboardArrowUp/>}</span></h5>
+                            <div className="option" style={{
+                                transition: '.2s',
+                                display: opened === '2' ? 'block' : "none"
+                            }}>
+                                <option selected>Specialization</option>
+                                <option>IT Technologies</option>
+                                <option>Business</option>
+                                <option>Medicine</option>
+                                <option>International Relationships</option>
+                                <option>Tourism</option>
+                            </div>
+                        </div>
+
+
+                        <div className='sel'>
+                            <h5 onClick={() => setOpened('3')}
+                                className='select'>Сountries <span>{ opened === '3' ? <MdKeyboardArrowDown/> : <MdKeyboardArrowUp/>}</span></h5>
+                            <div className="option" style={{
+                                transition: '.2s',
+                                display: opened === '3' ? 'block' : "none"
+                            }}>
+                                <option selected>Сountries</option>
+                                <option>America</option>
+                                <option>Germany</option>
+                                <option>Indonesia</option>
+                                <option>South Korea</option>
+                            </div>
+
+                        </div>
                     </div>
 
                     <div className='uni'>
@@ -40,15 +84,17 @@ const UnitedStates = () => {
                             universities.map(el => (
                                 <div onClick={() => navigate('/university')} className="uni--block">
                                     <img src={el.img} alt='#'/>
-                                    <div className='uni--block__info'>
-                                        <h2>Name:</h2>
-                                        <h1>{el.title}</h1>
+                                    <div className="fl">
+                                        <div className='uni--block__info'>
+                                            <h2>Name:</h2>
+                                            <h1>{el.title}</h1>
+                                        </div>
+                                        <div className='uni--block__info'>
+                                            <h2>Location <span><TfiLocationPin/></span></h2>
+                                            <h3>{el.loc}</h3>
+                                        </div>
                                     </div>
-                                    <div className='uni--block__info'>
-                                        <h2>Location <span><TfiLocationPin/></span></h2>
-                                        <h3>{el.loc}</h3>
-                                    </div>
-                                    <div className='uni--block__info'>
+                                    <div className='uni--block__info del'>
                                         <h2>Age:</h2>
                                         <h3>{el.acc}</h3>
                                     </div>
