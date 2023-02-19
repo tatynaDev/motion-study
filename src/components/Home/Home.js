d42277bee54b8d1f923affb94ad5507cd88c0c0c
Ademir__Dev <gaparov866@gmail.com>
2023-02-20 01:24:50 +0600

    ww

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,6 +30,31 @@ const Home = () => {
         speed: 500,
         slidesToShow: 2.1,
         slidesToScroll: 2.1,
+        responsive: [{
+            breakpoint: 1024, settings: {
+                slidesToShow: 3, slidesToScroll: 3,
+                infinite: true, dots: false
+            }
+        },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2, initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 576, settings: {
+                    slidesToShow: 1, slidesToScroll: 1,
+                    initialSlide: 1
+                }
+            }, {
+                breakpoint: 480, settings: {
+                    slidesToShow: 1, slidesToScroll: 1
+                }
+            }
+        ]
     }
     const [link, setLink] = useState(false);
 
